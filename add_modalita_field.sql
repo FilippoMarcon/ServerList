aa2136eaade8d2522c868b6db930a519003307f4
Filippo Marcon <f.marcon@donboscosandona.it>
2025-09-27 15:32:28 +0200

    Da backuppare server.php

--- add_modalita_field.sql
+++
@@ -1,5 +0,0 @@
--- Aggiunge il campo modalità alla tabella sl_servers
-ALTER TABLE `sl_servers` ADD COLUMN `modalita` TEXT DEFAULT NULL COMMENT 'Modalità di gioco del server (JSON array)';
-
--- Aggiorna i server esistenti con modalità di default
-UPDATE `sl_servers` SET `modalita` = '["Survival", "Adventure"]' WHERE `modalita` IS NULL;
