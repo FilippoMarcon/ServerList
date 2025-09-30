e51b47e52651fe21bb6c33bc2eff2bbf0b4d3662
Filippo Marcon <f.marcon@donboscosandona.it>
2025-09-30 21:43:59 +0200

    Fix Navbar admin

--- admin.php
+++ admin.php
@@ -960,13 +960,11 @@ include 'header.php';
                             include_dashboard();
                     }
                     ?>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</div>
+</div>
+</div>
+</div>
 <script>
 // Funzioni AJAX globali
 function makeAjaxRequest(action, data, callback) {
